197cc203cf5a98327530a321d11c57f4fd08dd12
Your Name <you@example.com>
2020-05-09 18:47:36 +0300

    Updates

---
+++ precache-manifest.6bf5758d29de11ecaad583fd9b41d101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19770ce3d5f8204daa6bfb069beed61",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "597bae42fa4be7837148",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.7d4f029e.chunk.css"
+  },
+  {
+    "revision": "3697bc85083b7373b8b7",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.1ba8b26d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.1ba8b26d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "597bae42fa4be7837148",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.723b2597.chunk.js"
+  },
+  {
+    "revision": "6590c4fb090a77339b8c",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.804d6d49.js"
+  }
+]);
